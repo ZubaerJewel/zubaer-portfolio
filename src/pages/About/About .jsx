756b20ac7f64983350248546e1c82e6b5663622e
ZubaerJewel <zubaer5jewel.16@gmail.com>
2023-07-03 14:52:47 +0600

    about me section done

--- src/pages/About/About .jsx
+++ src/pages/About/About .jsx
@@ -1,109 +1,58 @@
-/* eslint-disable react/no-unescaped-entities */
 
-import { FaFacebook, FaGithub, FaInstagram } from "react-icons/fa";
-// import { Link } from "react-router-dom";
+import "animate.css";
+import {  FaFacebook, FaTwitter, FaLinkedin } from "react-icons/fa";
+import Header from "./Header";
+import "animate.css";
 
-const About = () => {
+const AboutMe = () => {
   return (
-    <div className="py-12 items-center" id="about">
-      <h1 className="text-3xl font-semibold text-center py-20 dark:text-[#8892B0]">
-        About Me 
-      </h1>
-
-      <div className="  mx-auto">
-        <div className=" md:flex-row md:pr-8">
-          <div className="relative  justify-center items-center md:w-1/2 py-12">
-            <img
-              src="https://i.ibb.co/fH4mhNq/Zubaer-Hossain300x350.jpg"
-              className="rounded-full h-80 w-80"
-            />
-            <a
-              href="https://www.facebook.com/profile.php?id=100068116817527"
-              className="absolute text-2xl dark:text-white dark:bg-black bg-white rounded-full text-sky-600 -mb-72 -ml-36 hover:text-4xl duration-300"
-            >
-              <FaFacebook></FaFacebook>
-            </a>
-            <a className="absolute text-2xl text-black rounded-full dark:bg-white -mb-80 hover:text-4xl duration-300">
-              <FaGithub></FaGithub>
-            </a>
-            <a
-              href="https://www.instagram.com/_y_e_a__s_i_n_/"
-              className="absolute text-2xl text-red-600 bg-black  rounded-full -mb-72 -mr-36 hover:text-4xl duration-300"
-            >
-              <FaInstagram></FaInstagram>
-            </a>
-          </div>
-          <div className="md:w-1/2 text-center md:pl-10">
-            <p className="text-lg mb-4 text-justify">
-              Hi, I am Zubaer Hossain. I'm a passionate web developer with a keen
-              eye for detail, and a drive to create elegant, efficient, and
-              user-friendly web app.I've honed my skills in a variety of
-              technologies, including ReactJs, Nodejs,ExpressJs,Firebase,
-              Javascript es6+ and MongoDb. Now I am learning NextJs.
-            </p>
-            <div className="mb-4 md:flex justify-around gap-5">
-              <div className="mb-2 md:w-1/2 h-48 p-4  border-[2px] border-[#3a1775]">
-                <h1 className="text-lg font-semibold">Name</h1>
-                <p className="dark:text-[#8892B0]">Zubaer Hossain</p>
-                <h1 className="text-lg font-semibold">Email</h1>
-                <p className="dark:text-[#8892B0]">zubaer5jewel.16@gmail.com</p>
-                <h1 className="text-lg font-semibold">Phone</h1>
-                <p className="dark:text-[#8892B0]">+8801839-666712</p>
-              </div>
-              <div className="mb-2 md:w-1/2 p-4 h-48 border-[2px] border-[#3a1775]">
-                <h1 className="text-lg font-semibold">Address</h1>
-                <p className="dark:text-[#8892B0]">Jashore, Bangladesh</p>
-                <h1 className="text-lg font-semibold">B.Sc in CSE</h1>
-                <p className="dark:text-[#8892B0]">
-                  
-                </p>
-                <h1 className="text-lg font-semibold">Freelance</h1>
-                <p className="dark:text-[#8892B0]">Available</p>
-              </div>
+    <div className="h-5/6" data-aos="fade-up">
+      <Header heading="About Me"></Header>
+      <div className="flex flex-col md:flex-row align-middle">
+        <div className="flex flex-col m-3 md:w-1/2">
+          <p className="text-6xl border rounded-lg shadow-lg p-6 mt-3 mb-3 animate__animated animate__slideInLeft animate__delay-2s">
+            Ahmed Nashif
+          </p>
+          <p className="text-3xl border rounded-lg shadow-lg p-6 mb-3 animate__animated animate__slideInLeft animate__delay-3s">
+            I am a Web Developer
+          </p>
+          <p className="text-2xl border rounded-lg shadow-lg p-6 mb-3 animate__animated animate__slideInLeft animate__delay-4s">
+            I have a passion for creating intuitive<br></br> and user-friendly
+            web experiences.
+          </p>
+        </div>
+        <div className="m-3 p-3 border shadow-lg rounded-lg md:w-1/2 animate__animated animate__slideInRight animate__delay-5s">
+          <p>
+            I am a Highly motivated web developer with a passion for creating
+            visually appealing and user friendly websites. I am a continuous
+            learner and always stay up to date with emerging web technologies.
+            Committed to delivering exceptional user experiences and driving
+            innovation in web development industry.I am constantly seeking
+            opportunities to challenge myself and push the boundaries of what
+            can be achieved on the web.
+          </p>
+          <div className="mt-3 ms-3 pt-6">
+            <h3 className="text-2xl font-bold mb-6">Follow Me</h3>
+            <div className="">
+              {/* TODO:  */}
+              <a href="https://web.facebook.com/nashif910" target="blank">
+                <FaFacebook className="btn btn-circle me-4"></FaFacebook>
+              </a>
+              <a href="https://twitter.com/AN361770" target="blank">
+                <FaTwitter className="btn btn-circle me-4"></FaTwitter>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/ahmed-nashif-9840941a0/"
+                target="blank"
+              >
+                <FaLinkedin className="btn btn-circle me-4"></FaLinkedin>
+              </a>
             </div>
           </div>
         </div>
       </div>
-
-      {/* What i do */}
-      <h1 className="text-center font-semibold -mt-16 opacity-30 text-6xl dark:text-[#8892B0]">
-        What I do?
-      </h1>
-      {/* <div className="md:flex justify-evenly my-12"> */}
-        {/* option - 1 */}
-        {/* <div>
-          <div className="relative flex  flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
-    <div
-        className="group relative cursor-pointer overflow-hidden bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10">
-        <span className="absolute top-10 z-0 h-20 w-20 rounded-full bg-sky-500 transition-all duration-300 group-hover:scale-[10]"></span>
-        <div className="relative z-10 mx-auto max-w-md">
-            <span className="grid h-20 w-20 place-items-center rounded-full bg-sky-500 transition-all duration-300 group-hover:bg-sky-400">
-               <img src="https://i.ibb.co/2tf1tmG/1.png" alt="" />
-            </span>
-            <div
-                className="space-y-6 pt-5 text-base leading-7 text-gray-600 transition-all duration-300 group-hover:text-white/90">
-                <p>Perfect for learning how the framework works, prototyping a new idea, or creating a demo to share
-                    online.</p>
-            </div>
-            <div className="pt-5 text-base font-semibold leading-7">
-                <p>
-                    <a href="#" className="text-sky-500 transition-all duration-300 group-hover:text-white">Read the docs
-                        &rarr;
-                    </a>
-                </p>
-            </div>
-        </div>
-    </div>
-</div>
-
-        </div> */}
-        {/* option - 1 */}
-        {/* <div>2</div> */}
-        {/* option - 1 */}
-        {/* <div>3</div> */}
-      {/* </div> */}
     </div>
   );
 };
 
-export default About;
+export default AboutMe;
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -21,7 +21,7 @@ const Projects = () => {
                 />
               </div>
               <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
-              <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center text-center transition-all duration-500 group-hover:translate-y-0">
+              <div className="absolute w-auto inset-0 flex translate-y-[60%] flex-col items-center justify-center text-center transition-all duration-500 group-hover:translate-y-0">
                 <h1 className="font-dmserif text-2xl font-bold text-white">
                   eating-well
                 </h1>
@@ -32,6 +32,14 @@ const Projects = () => {
                 </p>
 
                 <div className="flex items-center gap-3">
+                  <a href="https://github.com/ZubaerJewel/eating-well-client">
+                    <button className="flex items-center gap-3 rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60 bg-gradient-to-r from-[#391263] to-yellow-600 hover:to-blue-500  justify-center focus:outline-none hover:bg-gray-600">
+                      <span>
+                        <FaGithub></FaGithub>
+                      </span>
+                      GitHub
+                    </button>
+                  </a>
                   <a href="https://github.com/ZubaerJewel/eating-well-client">
                     <button className="flex items-center gap-3 rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60 bg-gradient-to-r from-[#391263] to-yellow-600 hover:to-blue-500  justify-center focus:outline-none hover:bg-gray-600">
                       <span>
@@ -107,10 +115,18 @@ const Projects = () => {
                 <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
                   <p className="text-semibold">Technology used:</p>
                   HTML, CSS, Tailwind, JavaScript, React, Node.js, MongoDB,
-                  Express.js, Firebase, Stripe, JWT, Tanstack Query, React Hook
+                  Express.js, Firebase, Stripe, JWT, React Query, React Hook
                   Form
                 </p>
                 <div className="flex items-center gap-3">
+                  <a href="https://github.com/ZubaerJewel/Yoga-Meditation-Client">
+                    <button className="flex items-center gap-3 rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60 bg-gradient-to-r from-[#391263] to-yellow-600 hover:to-blue-500  justify-center focus:outline-none hover:bg-gray-600">
+                      <span>
+                        <FaGithub></FaGithub>
+                      </span>
+                      GitHub
+                    </button>
+                  </a>
                   <a href="https://github.com/ZubaerJewel/Yoga-Meditation-Client">
                     <button className="flex items-center gap-3 rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60 bg-gradient-to-r from-[#391263] to-yellow-600 hover:to-blue-500  justify-center focus:outline-none hover:bg-gray-600">
                       <span>
